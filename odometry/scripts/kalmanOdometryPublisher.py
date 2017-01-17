d6fd40bf409a39738ef28bbdc95d24c4f2679f55
cunnia3 <andrew64ce@gmail.com>
2017-01-17 16:46:28 -0500

    Tweaked kalman odometry publisher to work more smoothly

--- odometry/scripts/kalmanOdometryPublisher.py
+++ odometry/scripts/kalmanOdometryPublisher.py
@@ -6,6 +6,7 @@ from sensor_msgs.msg import Imu
 from nav_msgs.msg import Odometry
 import tf
 from std_msgs.msg import Int16
+import copy
 
 class ExtendedWMRKalmanFilter:
     """ Class to predict a wheeled mobile robot state [dtheta_l, dtheta_r,x,y,theta]
@@ -19,7 +20,7 @@ class ExtendedWMRKalmanFilter:
             @k1_l, k2_l motor constants for left motor
             @k1_r, k2_r motor constants for right motor
             @dt the time in between predicts """
-            
+        print "CREATING EKF"            
         self.r = r
         self.l = l
         self.Q = Q
@@ -28,7 +29,7 @@ class ExtendedWMRKalmanFilter:
         self.k2_l = k2_l
         self.k1_r = k1_r
         self.k2_r = k2_r
-        self.current_state_estimate = starting_state
+        self.current_state_estimate = copy.deepcopy(starting_state)
         self.current_prob_estimate = np.zeros([starting_state.shape[0], starting_state.shape[0]])
         self.dt = dt
         
@@ -39,18 +40,19 @@ class ExtendedWMRKalmanFilter:
     def predict(self, u):
         """ Predict new state from old state and command
         @u [v_l v_r], Step without sensing """
+
         dtheta_l = self.current_state_estimate[0,0]
         dtheta_r = self.current_state_estimate[1,0]
         theta = self.current_state_estimate[4,0]
         
-        self.A = np.array([[1+self.dt*self.k1_l, 0, 0, 0, 0],\
-                           [0, 1+self.dt*self.k1_r, 0, 0, 0],\
+        self.A = np.array([[1+self.dt*self.k2_l, 0, 0, 0, 0],\
+                           [0, 1+self.dt*self.k2_r, 0, 0, 0],\
                            [self.dt*(self.r)/2*math.cos(theta), self.dt*self.r/2*math.cos(theta), 1, 0, -math.sin(theta)*self.dt*(dtheta_r+dtheta_l)],\
                            [self.dt*(self.r)/2*math.sin(theta), self.dt*self.r/2*math.sin(theta), 0, 1, math.cos(theta)*self.dt*(dtheta_r+dtheta_l)],\
                            [-self.dt*self.r/self.l, self.dt*self.r/self.l, 0, 0, 1]])
                            
-        self.B = np.array([[self.dt*self.k2_l, 0],\
-                           [0, self.dt*self.k2_r],\
+        self.B = np.array([[self.dt*self.k1_l, 0],\
+                           [0, self.dt*self.k1_r],\
                            [0, 0],\
                            [0, 0],\
                            [0, 0]])
@@ -85,16 +87,18 @@ class StatePredictionNode:
               [0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0]])
-        R = np.eye(2) * .01
+        R = np.eye(2) * .15
         starting_state = np.zeros([5,1])
-        self.r = .9424
-        self.l = .57
-        self.ekf = ExtendedWMRKalmanFilter(self.r, self.l, Q, R, 2.3364e-04, .65, 2.3364e-04, .65,starting_state, .02)
+        self.r = .15
+        self.l = .55
+        self.ekf = ExtendedWMRKalmanFilter(self.r, self.l, Q, R, 2.3364e-04, -.65, 2.0624e-04, -.6189, starting_state, .05)
         self.pub = rospy.Publisher('odometry', Odometry, queue_size=1)
         rospy.Subscriber("/imu1", Imu, self._imu1Callback)
         rospy.Subscriber("/imu2", Imu, self._imu2Callback)
         rospy.Subscriber("left_voltage_pwm", Int16, self._leftMotorCallback) 
         rospy.Subscriber("right_voltage_pwm", Int16, self._rightMotorCallback)
+
+        self.f = open('ekf_data.csv', 'w')
         
     def _leftMotorCallback(self, data):
         self.control_voltages[0] = data.data
@@ -104,42 +108,48 @@ class StatePredictionNode:
         
     def _imu1Callback(self, data):
         self.vl = data.angular_velocity.z
-        if abs(self.vl) < .02:
+        if abs(self.vl) < .1:
             self.vl = 0
         
     def _imu2Callback(self, data):
         self.vr = data.angular_velocity.z
-        if abs(self.vr) < .02:
+        if abs(self.vr) < .1:
             self.vr = 0        
 
+    def _writeToFile(self, state, measurement):
+        self.f.write(str(state[0,0]) + ',' + str(state[1,0]) + ','+  str(state[2,0]) +  ',' + str(state[3,0]) +  ',' + str(state[4,0]) + ',' + str(measurement[0,0]) + ',' + str(measurement[1,0])  +  '\n') 
+
     def loop(self):
-        r = rospy.Rate(50)
+        r = rospy.Rate(20)
         while not rospy.is_shutdown():
-            print self.ekf.current_state_estimate[4]
+            if abs(self.vl) > .05 and abs(self.vr) > .05:
+                print self.ekf.current_state_estimate
+
             ## USE KALMAN FILTER
             self.ekf.predict(self.control_voltages)
             self.ekf.measurement_update(np.array([[self.vl], [self.vr]]))
-            
+            state = copy.deepcopy(self.ekf.current_state_estimate)
+            self._writeToFile(state, np.array([[self.vl], [self.vr]]))
             ## BUILD AND SEND MSG
-            quaternion = tf.transformations.quaternion_from_euler(0, 0, self.ekf.current_state_estimate[4])
+            quaternion = tf.transformations.quaternion_from_euler(0, 0, state[4])
             
             msg = Odometry()
             msg.header.stamp = rospy.Time.now()
-            msg.pose.pose.position.x = self.ekf.current_state_estimate[2]
-            msg.pose.pose.position.y = self.ekf.current_state_estimate[3]
-            msg.pose.pose.position.z = self.ekf.current_state_estimate[0]
+            msg.pose.pose.position.x = state[2]
+            msg.pose.pose.position.y = state[3]
+            msg.pose.pose.position.z = 0
             msg.pose.pose.orientation.x = quaternion[0]
             msg.pose.pose.orientation.y = quaternion[1]
             msg.pose.pose.orientation.z = quaternion[2]
             msg.pose.pose.orientation.w = quaternion[3]            
             
-            msg.twist.twist.linear.x = (self.r/2)*(self.ekf.current_state_estimate[0]\
-                + self.ekf.current_state_estimate[1])*math.cos(self.ekf.current_state_estimate[4])
-            msg.twist.twist.linear.y = (self.r/2)*(self.ekf.current_state_estimate[0]\
-                + self.ekf.current_state_estimate[1])*math.sin(self.ekf.current_state_estimate[4])
+            msg.twist.twist.linear.x = (self.r/2)*(state[0]\
+                + state[1])*math.cos(state[4])
+            msg.twist.twist.linear.y = (self.r/2)*(state[0]\
+                + state[1])*math.sin(state[4])
             msg.twist.twist.linear.z = 0
-            msg.twist.twist.angular.z = (self.r/self.l)*(self.ekf.current_state_estimate[1]\
-                - self.ekf.current_state_estimate[0])
+            msg.twist.twist.angular.z = (self.r/self.l)*(state[1]\
+                - state[0])
             
             self.pub.publish(msg)
             r.sleep()
