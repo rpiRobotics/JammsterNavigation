f1d228425024ca2feee8bc4829d8b5a0d00623e0
cunnia3 <andrew64ce@gmail.com>
2017-01-26 14:12:27 -0500

    Improved go to goal controller

--- base_controller/scripts/go_to_goal.py
+++ base_controller/scripts/go_to_goal.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jan 17 18:55:13 2017
@@ -21,7 +22,7 @@ class GoToGoal:
         rospy.Subscriber("/goal", Pose, self._goalCallback)
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
         
-        self.my_pid = PID(P=4.0, I=0.0, D=1.0, Derivator=0, Integrator=1, Integrator_max=10, Integrator_min=-10)
+        self.my_pid = PID(P=-2.0, I=0.0, D=0.0, Derivator=0, Integrator=0, Integrator_max=2, Integrator_min=-2)
         self.v = 0
         self.w = 0
         
@@ -33,14 +34,15 @@ class GoToGoal:
         self.desired_y = 0
         self.desired_bearing = 0        
         
-        self.close_enough = False
+        self.close_enough = True
 
     def _goalCallback(self, data):
+        print "Got new goal"
         self.desired_x = data.position.x
         self.desired_y = data.position.y
 
     def _odomCallback(self, data):
-        quat = data.pose.pose.orientation
+        quat = (data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w)
         euler = tf.transformations.euler_from_quaternion(quat)
         self.current_bearing = euler[2]
         
@@ -50,24 +52,30 @@ class GoToGoal:
         if math.sqrt((self.current_x - self.desired_x)**2 + (self.current_y - self.desired_y)**2) > .2:
             self.close_enough = False
         else:
-            self.close_enough = True
-            
+            self.close_enough = True            
         return
 
     def spin(self):
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
-            self.my_pid.set_point(self.desired_bearing)
+            self.my_pid.setPoint(self.desired_bearing)
             self.w=self.my_pid.update(self.current_bearing)
             
-            if self.close_enough:
+            if self.desired_x == 0 and self.desired_y ==0:
+                self.v = 0
+                self.w = 0 
+            elif self.close_enough:
                 self.v = 0
                 self.w = 0
             else:
-                self.v = .2
+                self.v = .0
                 
+            print "%.2f, %.2f, %.2f" %(self.my_pid.getError(), self.desired_bearing, math.sqrt((self.current_x - self.desired_x)**2 + (self.current_y - self.desired_y)**2))
             msg = Twist()
             msg.linear.x = self.v
             msg.angular.z = self.w
             self.pub.publish(msg)            
             r.sleep()
+
+go_goal_node = GoToGoal()
+go_goal_node.spin()
